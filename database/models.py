81a9e6c0009d01eaef96dd4d863797d0c1031dd5
Carlos Alberto Baltazar Hinojosa <A840159943@my.uvm.edu.mx>
2024-07-17 22:32:42 -0600

    Ejecución de la migración inicial con la que el usuario deberá interactuar.

--- database/models.py
+++ database/models.py
@@ -1,5 +1,15 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class UserDemo(Base):
+    __tablename__ = 'user_demo'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(50))
+    address: Mapped[str] = mapped_column(String(100))
+    
