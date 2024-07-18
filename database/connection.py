4b1d5828c8aafd3467c6e8d7ba7357b708fd08ed
Carlos Alberto Baltazar Hinojosa <A840159943@my.uvm.edu.mx>
2024-07-17 19:22:12 -0600

    Configuracion de la conexion a la db usando variables de entorno.

--- database/connection.py
+++ database/connection.py
@@ -1,1 +1,26 @@
-from sqlalchemy.orm import Session
+# Environment variables
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+HOST     = os.getenv("HOST")
+PORT     = os.getenv("PORT")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+DATABASE = os.getenv("DATABASE")
+
+
+# SQLAlchemy and database session configuration
+from database.models import Base
+from sqlalchemy.orm  import Session
+from sqlalchemy      import create_engine
+
+DATABASE_URI = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+engine = create_engine(DATABASE_URI, future=True)
+
+def get_session() -> Session:
+    try:
+        return Session(engine)
+    except Exception as e:
+        raise ConnectionError(f'Error en la conexión a la base de datos {e}')
+    
